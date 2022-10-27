b593cfbcd1145f487b0fd7a5510b965c5ceff6bc
hugin0 <131902+Hugin0@users.noreply.github.com>
2022-10-27 18:24:36 -0400

    Initial commit of my sofle, custom modified, miryoku layout
    Includes:
    	mapping of keys not accounted for in the miryoku layout
    	custom number layout in the num layer
    	matching changes to the Symbol and Function layers to match the Number layer
    	modifications to the Tap layer to use it for gaming
    
    see comments in custom_config.h for more details.

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,71 @@
 
 #pragma once
 
+/* This sets up just the numbers to be reversed so that you still operate one with your pointer just like an actual number pad.   */
+#define MIRYOKU_LAYER_NUM \
+KC_LBRC,           KC_9,              KC_8,              KC_7,              KC_RBRC,           U_NA,              DF(U_BASE),        DF(U_EXTRA),       DF(U_TAP),         QK_BOOT,           \
+KC_SCLN,           KC_6,              KC_5,              KC_4,              KC_EQL,            U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_GRV,            KC_3,              KC_2,              KC_1,              KC_BSLS,           U_NA,              DF(U_NUM),         DF(U_NAV),         KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+/* Mimic the symbol layer, so that the symbols line up with the numbers on the reversed number pad */
+#define MIRYOKU_LAYER_SYM \
+KC_LCBR,           KC_LPRN,           KC_ASTR,           KC_AMPR,           KC_RCBR,           U_NA,              DF(U_BASE),        DF(U_EXTRA),       DF(U_TAP),         QK_BOOT,           \
+KC_COLN,           KC_CIRC,           KC_PERC,           KC_DLR,            KC_PLUS,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_TILD,           KC_HASH,           KC_AT,             KC_EXLM,           KC_PIPE,           U_NA,              DF(U_SYM),         DF(U_MOUSE),       KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+/* Lastly align the fuction numbers with the numbers on the num layer so that you can reuse the muscle memory. */
+#define MIRYOKU_LAYER_FUN \
+KC_F12,            KC_F9,             KC_F8,             KC_F7,             KC_PSCR,           U_NA,              DF(U_BASE),        DF(U_EXTRA),       DF(U_TAP),         QK_BOOT,           \
+KC_F11,            KC_F6,             KC_F5,             KC_F4,             KC_SLCK,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_F10,            KC_F3,             KC_F2,             KC_F1,             KC_PAUS,           U_NA,              DF(U_FUN),         DF(U_MEDIA),       KC_ALGR,           U_NA,              \
+U_NP,              U_NP,              KC_APP,            KC_SPC,            KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+
+
+// Sofle Specific Config
+#if defined (KEYBOARD_sofle)
+
+#define XXX KC_NO
+#define ESC_CTL CTL_T(KC_ESC)
+
+/*
+  Define the rest of the keys for the default mapping in case someone else needs to use my keyboard/PC so that more of the legends match up, and it should feel more natural, save for the HRMs.
+  Mimics my previous keymaping before adopting miryoku
+*/
+#define MIRYOKU_MAPPING(                                                 \
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
+)\
+LAYOUT(\
+KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,\
+KC_TAB,  K00,  K01,  K02,  K03,  K04,                K05,  K06,  K07,  K08,  K09,  KC_BSPC,\
+ESC_CTL, K10,  K11,  K12,  K13,  K14,                K15,  K16,  K17,  K18,  K19,  KC_SCLN,\
+KC_LSFT, K20,  K21,  K22,  K23,  K24, XXX,      XXX, K25,  K26,  K27,  K28,  K29,  KC_RSFT,\
+          XXX, XXX,  K32,  K33,  K34,                K35,  K36,  K37,  XXX,  XXX\
+)
+
+/*
+  Make use of the tap layer to work for FPS games with minimal special configurations
+  Here we:
+    * Move escape up to replace GRAVE and put GRAVE where the BACKSPACE key, would normally be.
+    * Add a key to one of the non-used keys to switch back to the normal base layer when we are done with a game.
+*/
+#define MIRYOKU_LAYERMAPPING_TAP(                                                 \
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
+)\
+LAYOUT(\
+KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,\
+KC_TAB,  K00,  K01,  K02,  K03,  K04,                K05,  K06,  K07,  K08,  K09,  KC_GRAVE,\
+KC_LCTL, K10,  K11,  K12,  K13,  K14,                K15,  K16,  K17,  K18,  K19,  KC_SCLN,\
+KC_LSFT, K20,  K21,  K22,  K23,  K24, XXX,      XXX, K25,  K26,  K27,  K28,  K29,  KC_RSFT,\
+         XXX,  XXX,  K32,  K33,  K34,                K35,  K36,  K37,  XXX,  DF(U_BASE) \
+)
+
+#endif
